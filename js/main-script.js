f254027cc6ee941b3f877f6ad4f0fef1b26d703f
Andre Azevedo <andremazevedo6@gmail.com>
2023-05-22 00:09:18 +0100

    finished robot and added keyboard controls

--- js/main-script.js
+++ js/main-script.js
@@ -11,16 +11,22 @@ var cameras = [];
 var materials = [];
 
 var controls;
+var gui;
+var stats;
+
+var clock, delta;
 
 var robot;
-var headSet, head, leftAntenna, rightAntenna;
-var leftArmSet, rigthArmSet, leftArm, rightArm, leftForearm, rightForearm;
+var headSet, head, leftAntenna, rightAntenna, leftEye, rightEye, headStep;
+var leftArmSet, rigthArmSet, leftArm, rightArm, leftForearm, rightForearm, leftPipe, rightPipe, leftArmStep, rightArmStep;
 var torso, abdomen, waist, leftWaistWheel, rightWaistWheel;
-var legSet, rightThigh, leftThigh, rightLeg, leftLeg, upperLeftLegWheel, lowerLeftLegWheel, uppperRightLegWheel, lowerRightLegWheel;
-var footSet, rightFoot, leftFoot, rightSidefoot, leftSidefoot;
+var legSet, rightThigh, leftThigh, rightLeg, leftLeg, upperLeftLegWheel, lowerLeftLegWheel, upperRightLegWheel, lowerRightLegWheel, legStep;
+var footSet, rightFoot, leftFoot, rightSidefoot, leftSidefoot, footStep;
 
 const headWidth = 8, headHeight = 8, headDepth = 8;
-const antennaWidth = 1, antennaHeight = 4, antennaDepth = 1;
+const antennaWidth = 1, antennaHeight = 6, antennaDepth = 2;
+const pipeRadius = 1, pipeHeight = 22;
+const eyeWidth = 2, eyeHeight = 1, eyeDepth = 1;
 const armWidth = 6, armHeight = 16, armDepth = 8;
 const forearmWidth = armWidth, forearmHeight = 6, forearmDepth = 24;
 const torsoWidth = 24, torsoHeight = 16, torsoDepth = 24;
@@ -148,24 +154,30 @@ function createRobot() {
     createHead();
 
     headSet = new THREE.Object3D();
+    headSet.userData = { positive: 0, negative: 0 };
     headSet.add(head);
     headSet.add(leftAntenna);
     headSet.add(rightAntenna);
-    // headSet.rotation.x = 0;
+    headSet.add(leftEye);
+    headSet.add(rightEye);
     headSet.position.set(0, abdomenHeight + torsoHeight - offset, 0);
 
     createLeftArm();
 
     leftArmSet = new THREE.Object3D();
+    leftArmSet.userData = { positive: 0, negative: 0 };
     leftArmSet.add(leftArm);
     leftArmSet.add(leftForearm);
+    leftArmSet.add(leftPipe);
     leftArmSet.position.set(- torsoWidth / 2 - armWidth / 2, abdomenHeight + torsoHeight, - torsoDepth / 2 + armDepth / 2);
 
     createRigthArm();
 
     rigthArmSet = new THREE.Object3D();
+    rigthArmSet.userData = { positive: 0, negative: 0 };
     rigthArmSet.add(rightArm);
     rigthArmSet.add(rightForearm);
+    rigthArmSet.add(rightPipe);
     rigthArmSet.position.set(torsoWidth / 2 + armWidth / 2, abdomenHeight + torsoHeight, - torsoDepth / 2 + armDepth / 2);
 
     createTorso();
@@ -181,27 +193,27 @@ function createRobot() {
     createFoot();
 
     footSet = new THREE.Object3D();
+    footSet.userData = { positive: 0, negative: 0 };
     footSet.add(leftFoot);
     footSet.add(rightFoot);
     footSet.add(leftSidefoot);
     footSet.add(rightSidefoot);
     // footSet.rotation.x = 0;
     footSet.position.set(0, - thighHeight - legHeight + footHeight / 2, 0);
-    console.log('footSet');
-    console.log(footSet.position);
 
     legSet = new THREE.Object3D();
+    legSet.userData = { positive: 0, negative: 0 };
     legSet.add(leftThigh);
     legSet.add(rightThigh);
     legSet.add(leftLeg);
     legSet.add(rightLeg);
     legSet.add(lowerLeftLegWheel);
     legSet.add(lowerRightLegWheel);
+    legSet.add(upperLeftLegWheel);
+    legSet.add(upperRightLegWheel);
     legSet.add(footSet);
     //legSet.rotation.x = Math.PI / 2;
     legSet.position.set(0, legDepth / 2,  thighHeight - abdomenDepth / 2);
-    console.log('legSet');
-    console.log(legSet.position);
 
     robot = new THREE.Object3D();
     robot.add(headSet);
@@ -224,10 +236,16 @@ function createHead() {
     head.position.set(0, headHeight / 2 + offset, 0); 
 
     leftAntenna = new THREE.Mesh(new THREE.BoxGeometry(antennaWidth, antennaHeight, antennaDepth), materials[3]);
-    leftAntenna.position.set(headWidth / 2 - antennaWidth, headHeight + offset + antennaHeight/2, 0);
+    leftAntenna.position.set(- headWidth / 2 - antennaWidth / 2, headHeight + offset, 0);
 
     rightAntenna = new THREE.Mesh(new THREE.BoxGeometry(antennaWidth, antennaHeight, antennaDepth), materials[3]);
-    rightAntenna.position.set(- headWidth / 2 + antennaWidth, headHeight + offset + antennaHeight/2, 0);
+    rightAntenna.position.set(headWidth / 2 + antennaWidth / 2, headHeight + offset, 0);
+
+    leftEye = new THREE.Mesh(new THREE.BoxGeometry(eyeWidth, eyeHeight, eyeDepth), materials[1]);
+    leftEye.position.set(- headWidth / 4, headHeight / 2 + eyeHeight / 2 + 1 + offset, headDepth / 2 - eyeDepth / 2 + offset);
+
+    rightEye = new THREE.Mesh(new THREE.BoxGeometry(eyeWidth, eyeHeight, eyeDepth), materials[1]);
+    rightEye.position.set(headWidth / 4, headHeight / 2 + eyeHeight / 2 + 1 + offset, headDepth / 2 - eyeDepth / 2 + offset);
 }
 
 function createTorso() {
@@ -280,13 +298,19 @@ function createLeg() {
 
     lowerLeftLegWheel = new THREE.Mesh(new THREE.CylinderGeometry(wheelRadius, wheelRadius, wheelHeight, 32), materials[2]);
     lowerLeftLegWheel.rotation.z = Math.PI / 2;
-    lowerLeftLegWheel.position.set(- abdomenWidth / 2 - wheelHeight / 2 - 1 + offset, - thighHeight - legHeight + wheelRadius + 1, legDepth / 2 - waistHeight / 2);
+    lowerLeftLegWheel.position.set(- abdomenWidth / 2 - 1 - wheelHeight / 2 + offset, - thighHeight - legHeight + wheelRadius + 1, legDepth / 2 - waistHeight / 2);
 
     lowerRightLegWheel = new THREE.Mesh(new THREE.CylinderGeometry(wheelRadius, wheelRadius, wheelHeight, 32), materials[2]);
     lowerRightLegWheel.rotation.z = Math.PI / 2;
-    lowerRightLegWheel.position.set(abdomenWidth / 2 + wheelHeight / 2 + 1 - offset, - thighHeight - legHeight + wheelRadius + 1, legDepth / 2 - waistHeight / 2);
-    console.log('lowerRightLegWheel');
-    console.log(lowerRightLegWheel.position);
+    lowerRightLegWheel.position.set(abdomenWidth / 2 + 1 + wheelHeight / 2 - offset, - thighHeight - legHeight + wheelRadius + 1, legDepth / 2 - waistHeight / 2);
+
+    upperLeftLegWheel = new THREE.Mesh(new THREE.CylinderGeometry(wheelRadius, wheelRadius, wheelHeight, 32), materials[2]);
+    upperLeftLegWheel.rotation.z = Math.PI / 2;
+    upperLeftLegWheel.position.set(- abdomenWidth / 2 - 1 - wheelHeight / 2 + offset, - thighHeight - (legHeight - footHeight) / 2, legDepth / 2 - waistHeight / 2);
+
+    upperRightLegWheel = new THREE.Mesh(new THREE.CylinderGeometry(wheelRadius, wheelRadius, wheelHeight, 32), materials[2]);
+    upperRightLegWheel.rotation.z = Math.PI / 2;
+    upperRightLegWheel.position.set(abdomenWidth / 2 + 1 + wheelHeight / 2 - offset, - thighHeight - (legHeight - footHeight) / 2, legDepth / 2 - waistHeight / 2);
 }
 
 function createRigthArm() {
@@ -297,6 +321,9 @@ function createRigthArm() {
 
     rightForearm = new THREE.Mesh(new THREE.BoxGeometry(forearmWidth, forearmHeight, forearmDepth), materials[0]);
     rightForearm.position.set(0, - armHeight - forearmHeight / 2, torsoDepth / 2 - armDepth / 2);
+
+    rightPipe = new THREE.Mesh(new THREE.CylinderGeometry(pipeRadius, pipeRadius, pipeHeight, 16), materials[1]);
+    rightPipe.position.set(0, - armHeight + pipeHeight / 2, - armDepth / 2 - pipeRadius);
 }
 
 function createLeftArm() {
@@ -307,6 +334,9 @@ function createLeftArm() {
 
     leftForearm = new THREE.Mesh(new THREE.BoxGeometry(forearmWidth, forearmHeight, forearmDepth), materials[0]);
     leftForearm.position.set(0, - armHeight - forearmHeight / 2,  torsoDepth / 2 - armDepth / 2);
+
+    leftPipe = new THREE.Mesh(new THREE.CylinderGeometry(pipeRadius, pipeRadius, pipeHeight, 16), materials[1]);
+    leftPipe.position.set(0, - armHeight + pipeHeight / 2, - armDepth / 2 - pipeRadius);
 }
 
 function createFoot() {
@@ -347,6 +377,62 @@ function handleCollisions(){
 function update(){
     'use strict';
 
+    footStep = (footSet.userData.positive - footSet.userData.negative) * Math.PI / 2 * delta;
+    footSet.rotation.x += footStep;
+
+    if (footSet.rotation.x < 0) {
+        footSet.rotation.x = 0;
+    }
+    else if (footSet.rotation.x > Math.PI / 2) {
+        footSet.rotation.x =  Math.PI / 2;
+    }
+
+    legStep = (legSet.userData.positive - legSet.userData.negative) * Math.PI / 2 * delta;
+    legSet.rotation.x += legStep;
+
+    if (legSet.rotation.x < 0) {
+        legSet.rotation.x = 0;
+    }
+    else if (legSet.rotation.x > Math.PI / 2) {
+        legSet.rotation.x =  Math.PI / 2;
+    }
+    
+    headStep = (headSet.userData.positive - headSet.userData.negative) * Math.PI * delta;
+    headSet.rotation.x += headStep;
+
+    if (headSet.rotation.x < - Math.PI) {
+        headSet.rotation.x = - Math.PI;
+    }
+    else if (headSet.rotation.x > 0) {
+        headSet.rotation.x =  0;
+    }
+
+    // if (headSet.userData.positive && !headSet.userData.negative && headSet.rotation.x < 0) {
+    //     headSet.rotation.x += Math.PI / 180;
+    // }
+    // else if (!headSet.userData.positive && headSet.userData.negative && headSet.rotation.x > - Math.PI) {
+    //     headSet.rotation.x -= Math.PI / 180;
+    // }
+
+    leftArmStep = (leftArmSet.userData.positive - leftArmSet.userData.negative) * armWidth * delta;
+    leftArmSet.position.x += leftArmStep;
+
+    if (leftArmSet.position.x < - torsoWidth / 2 - armWidth / 2) {
+        leftArmSet.position.x = - torsoWidth / 2 - armWidth / 2;
+    }
+    else if (leftArmSet.position.x >  - torsoWidth / 2 + armWidth / 2) {
+        leftArmSet.position.x =  - torsoWidth / 2 + armWidth / 2;
+    }
+
+    rightArmStep = (rigthArmSet.userData.positive - rigthArmSet.userData.negative) * armWidth * delta;
+    rigthArmSet.position.x += rightArmStep;
+
+    if (rigthArmSet.position.x < torsoWidth / 2 - armWidth / 2) {
+        rigthArmSet.position.x = torsoWidth / 2 - armWidth / 2;
+    }
+    else if (rigthArmSet.position.x > torsoWidth / 2 + armWidth / 2) {
+        rigthArmSet.position.x = torsoWidth / 2 + armWidth / 2;
+    }
 }
 
 /////////////
@@ -370,15 +456,18 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
+    stats = new Stats();
+    document.body.appendChild( stats.dom );
+
     createMaterials();
 
     createScene();
 
     createCameras();
-    
-    render();
 
-    var gui = new dat.GUI();
+    clock = new THREE.Clock();
+
+    gui = new dat.GUI();
 
     var camerasFolder = gui.addFolder( 'Cameras' );
 
@@ -419,8 +508,13 @@ function init() {
     var footFolder = gui.addFolder("Foot");
     footFolder.add(footSet.rotation, 'x', 0, Math.PI / 2);
     footFolder.open();
+
+    stats.begin();
+    render();
+    stats.end();
     
     window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("keyup", onKeyUp);
 }
 
 /////////////////////
@@ -429,7 +523,12 @@ function init() {
 function animate() {
     'use strict';
 
+    delta = clock.getDelta();
+    
+    update();
     render();
+
+    stats.update();
 
     requestAnimationFrame(animate);
 }
@@ -458,16 +557,56 @@ function onKeyDown(e) {
         case 51: //3 Top Camera
             camera = cameras[2];
             break;
-        case 52: //3 Isometric Orthographic Camera
+        case 52: //4 Isometric Orthographic Camera
             camera = cameras[3];
             break;
-        case 53: //3 Isometric Prespective Camera
+        case 53: //5 Isometric Prespective Camera
             camera = cameras[4];
             break;
-        case 54: //A Toggle Wireframe
+        case 54: //6 Toggle Wireframe
             materials.forEach(material => {material.wireframe = !material.wireframe});
             break;
-        }
+        case 65: //A
+        case 97: //a
+            footSet.userData.positive = 1;
+            break;
+        
+        case 68: //D
+        case 100: //d
+            leftArmSet.userData.positive = 1;
+            rigthArmSet.userData.negative = 1;
+            break;
+        case 69: //E    
+        case 101: //e
+            leftArmSet.userData.negative = 1;
+            rigthArmSet.userData.positive = 1;
+            break;
+
+        case 70: //F
+        case 102: //f
+            headSet.userData.negative = 1;
+            break;
+        
+
+        case 81: //Q
+        case 113: //q
+            footSet.userData.negative = 1;
+            break;
+
+        case 82: //R
+        case 114: //r
+            headSet.userData.positive = 1;
+            break;
+
+        case 83: //S
+        case 115: //s
+            legSet.userData.positive = 1;
+            break;
+        case 87: //W
+        case 119: //w
+            legSet.userData.negative = 1;
+            break;    
+    }
 
 }
 
@@ -476,5 +615,42 @@ function onKeyDown(e) {
 ///////////////////////
 function onKeyUp(e){
     'use strict';
+
+    switch (e.keyCode) {
+        case 65: //A
+        case 97: //a
+            footSet.userData.positive = 0;
+            break;
+        case 68: //D
+        case 100: //d
+            leftArmSet.userData.positive = 0;
+            rigthArmSet.userData.negative = 0;
+            break;
+        case 69: //E    
+        case 101: //e
+            leftArmSet.userData.negative = 0;
+            rigthArmSet.userData.positive = 0;
+            break;
+        case 70: //F
+        case 102: //f
+            headSet.userData.negative = 0;
+            break;
+        case 81: //Q
+        case 113: //q
+            footSet.userData.negative = 0;
+            break;
+        case 82: //R
+        case 114: //r
+            headSet.userData.positive = 0;
+            break;
+        case 83: //S
+        case 115: //s
+            legSet.userData.positive = 0;
+            break;
+        case 87: //W
+        case 119: //w
+            legSet.userData.negative = 0;
+            break;
+    }
 
 }
